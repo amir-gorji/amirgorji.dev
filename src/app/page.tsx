ee1453fffee06fcd6a97f01bd1bbf4c88468a41d
Amir Gorji <amir1gorji@gmail.com>
2026-05-07 00:04:05 +0200

    Forced the main page to be static

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { SocialLinks } from '@/components/SocialLinks';
 import { getAllPosts } from '@/lib/posts';
 import Image from 'next/image';
 
+export const dynamic = 'force-static';
+
 export default async function Home() {
   const posts = await getAllPosts();
 
